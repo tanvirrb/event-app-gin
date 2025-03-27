fb74c8f996dbc86713e5ea78e5afea44c7e0f0e8
Tanvir Islam <tanvir.rb@outlook.com>
2025-03-27 14:13:18 +0000

    feat(mongodb): mongodb added

---
+++ src/configs/mongo.go
@@ -0,0 +1,30 @@
+package configs
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+func ConnectDB() *mongo.Client {
+	uri := "mongodb://localhost:27017"
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client
+}
+
+func GetCollection(client *mongo.Client, collection string) *mongo.Collection {
+	return client.Database("event-app-db").Collection(collection)
+}
--- src/main.go
+++ src/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/tanvirrb/event-app-go/src/configs"
 	"github.com/tanvirrb/event-app-go/src/router" // Update this import path to match your project structure
 )
 
 func main() {
+	configs.ConnectDB()
 	route := gin.Default()
 	eventsRoutes := route.Group("/events")
 	router.RegisterEventsRoutes(eventsRoutes)
