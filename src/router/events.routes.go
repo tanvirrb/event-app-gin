946057ca224a5fff1454c358c6e71470c122c8b2
Tanvir Islam <tanvir.rb@outlook.com>
2025-04-14 17:41:19 +0100

    feat: add POST endpoint for uploading assets in events routes

--- src/router/events.routes.go
+++ src/router/events.routes.go
@@ -1,10 +1,17 @@
 package router
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/tanvirrb/event-app-gin/src/configs"
 	"github.com/tanvirrb/event-app-gin/src/events"
 )
+
+type Assets struct {
+	Data map[string]interface{} `json:"-"`
+}
 
 func RegisterEventsRoutes(router *gin.RouterGroup) {
 	collection := configs.GetCollection("events")
@@ -17,4 +24,18 @@ func RegisterEventsRoutes(router *gin.RouterGroup) {
 	router.GET("", controller.GetAll)
 	router.PUT("/:id", controller.Update)
 	router.DELETE("/:id", controller.Delete)
+	router.POST("/assets", func(ctx *gin.Context) {
+		var assets Assets
+		err := ctx.ShouldBindJSON(&assets.Data)
+		fmt.Printf("ASSETS: %v", assets.Data)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		ctx.JSON(http.StatusCreated, gin.H{
+			"message": "Assets uploaded successfully",
+		})
+	})
 }
